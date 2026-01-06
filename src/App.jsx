7534cead3a021bc1172200c456f057e0b0ea39bd
Joseph Pal <josephpal27@gmail.com>
2026-01-06 23:30:48 +0530

    Home Banner added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import { Outlet } from "react-router-dom"
+import MyNavbar from "./components/Navbar"
 
 const App = () => {
 
   return (
     <>
       {/* <PageTitle/> */}
-      {/* <MyNavbar/> */}
+      <MyNavbar/>
       <Outlet />
       {/* <Footer/> */}
       {/* <ScrollToTop/> */}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,48 @@
-import MyNavbar from "../components/Navbar"
-
 
 
 const Home = () => {
   return (
     <>
-      <MyNavbar/>
-      <h1 className="text-red-300">We Connect</h1>
+      {/* Banner */}
+      <section className="relative">
+        <img src="/images/home-banner.png" alt="Home Banner" className="
+        w-[100%] h-[88vh] object-cover
+        " />
+        <div className="
+        absolute top-0 left-0 w-[100%] h-[100%] px-[7%] d-flex flex-col justify-center
+        ">
+          <h1 className="
+          text-[1rem] lg:text-[3.2rem] xl:text-[3.6rem] 2xl:text-[4rem]
+          text-[#fff] 
+          font-[600] 
+          mt-[1rem] lg:mt-[0.5rem] xl:mt-[0.8rem] 2xl:mt-[1rem]
+          ">
+            Connecting <span>Businesses</span> <br /> and People Through <span>Smart Solutions</span>
+          </h1>
+          <p className="
+          w-[45%] 
+          text-[#fff] 
+          text-[1.2rem] lg:text-[0.95rem] xl:text-[1.1rem] 2xl:text-[1.2rem]
+          font-[600] 
+          mt-[2rem] lg:mt-[1.5rem] xl:mt-[1.8rem] 2xl:mt-[2rem]
+          ">
+            From high-performance technology and cybersecurity to trusted visa and passport services, we enable seamless connectivity, secure systems, and global mobility.
+          </p>
+          <button className="
+          w-max 
+          bg-[#fff]
+          text-[#0764f9] 
+          text-[1.2rem] lg:text-[0.95rem] xl:text-[1.1rem] 2xl:text-[1.2rem]
+          font-[600] 
+          px-[2.8rem] lg:px-[2.4rem] xl:px-[2.6rem] 2xl:px-[2.8rem]
+          py-[1rem] lg:py-[0.8rem] xl:py-[0.9rem] 2xl:py-[1rem]
+          rounded-[30px] 
+          mt-[2rem] lg:mt-[1.5rem] xl:mt-[1.8rem] 2xl:mt-[2rem]
+          ">
+            Explore Services
+          </button>
+        </div>
+      </section>
     </>
   )
 }
