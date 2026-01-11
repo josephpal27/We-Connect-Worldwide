74ea501e317888813aad5ff916a6b0d86cc56aab
Joseph Pal <josephpal27@gmail.com>
2026-01-11 22:14:14 +0530

    Get In Touch Updated

--- src/components/Consultation.jsx
+++ src/components/Consultation.jsx
@@ -17,14 +17,23 @@ const Consultation = () => {
                     Building <span className="text-[#fff]">Smarter</span> Businesses for a Connected <span className="text-[#fff]">World</span>
                 </h6>
                 <p className="
-                    text-[#fff] font-[500] text-center
+                    text-[#fff] font-[600] text-center
                     text-[1.3rem]
                     w-[50%]
                     mt-[1rem]
                 ">
                     Through thoughtful collaboration, we help brands move forward with clarity, confidence, and consistency.
                 </p>
-                <button>
+                <button className="
+                    text-[#0764f9] bg-[#fff] rounded-[40px] font-[700]
+                    py-[1.2rem]
+                    px-[2.5rem]
+                    mt-[2rem]
+                    text-[1.2rem]
+                    hover:translate-y-[-5px]
+                    shadow-[0_5px_15px_rgba(0,0,0,0.35)]
+                    transition
+                ">
                     Schedule a Consultation
                 </button>
 
