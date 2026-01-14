f902402a103fe1ef15f408d6a7be0706ac3705b6
Joseph Pal <josephpal27@gmail.com>
2026-01-14 23:18:02 +0530

    Contact Form Updated

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -2,9 +2,48 @@
 const ContactForm = () => {
     return (
         <>
-            <form action="">
-
-            </form>
+            <div className="
+                bg-[#0764f9]
+                p-[3rem]
+                rounded-[40px]
+            ">
+                <span className="
+                    text-[#fff] font-[700]
+                    text-[1.8rem]
+                ">
+                    Get In Touch
+                </span>
+                <p className="
+                    text-[1.2rem]
+                    mt-[1rem]
+                    text-[#eee]
+                ">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod consectetur tempor incididunt adipiscing ut labore et dolore magna aliqua.
+                </p>
+                <form action="" className="
+                    contact-form
+                    mt-[2.7rem]
+                ">
+                    <label htmlFor="name">Full Name</label>
+                    <input type="text" name="name" id="name" required />
+                    <label htmlFor="email">Email Id</label>
+                    <input type="email" name="email" id="email" required />
+                    <label htmlFor="msg">Message</label>
+                    <textarea name="message" id="msg" required></textarea>
+                    <button type="submit" className="
+                        bg-[#fff] text-[#0764f9]
+                        py-[1rem]
+                        px-[3rem]
+                        text-[1.2rem]
+                        font-[600]
+                        rounded-full
+                        transition
+                        hover:translate-y-[-3px]
+                    ">
+                        Send Message
+                    </button>
+                </form>
+            </div>
         </>
     )
 }
