acc2c78efe82fec1a5ddab053033af11b11173b7
Joseph Pal <josephpal27@gmail.com>
2026-01-19 23:36:30 +0530

    Services Responsiveness added

--- src/components/OurServices.jsx
+++ src/components/OurServices.jsx
@@ -3,20 +3,20 @@ const OurServices = () => {
     return (
         <>
             <section className="
-                py-[1rem]
+                pt-[1rem] lg:pt-[0] xl:pt-[0.5rem] 2xl:pt-[0.5rem]
                 px-[7%]
-                pb-[5rem]
+                pb-[5rem] lg:pb-[4rem] xl:pb-[4.5rem] 2xl:pb-[5rem]
             ">
                 <h3 className="
                     text-center text-[#0764f9] font-[600]
-                    text-[3.5rem]
+                    text-[2rem] lg:text-[2.7rem] xl:text-[3.1rem] 2xl:text-[3.5rem]
                 ">
                     Services
                 </h3>
                 <p className="
                     text-center text-[#000] font-[500]
-                    text-[1.3rem]
-                    mt-[1rem]
+                    text-[1.3rem] lg:text-[1.05rem] xl:text-[1.2rem] 2xl:text-[1.3rem]
+                    mt-[1rem] lg:mt-[0.8rem] xl:mt-[0.9rem] 2xl:mt-[1rem]
                     mx-auto
                     w-[50%]
                 ">
