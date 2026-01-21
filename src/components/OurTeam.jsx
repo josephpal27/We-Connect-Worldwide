59bf767503fbaa4c7183fb82b99618176efeef59
Joseph Pal <josephpal27@gmail.com>
2026-01-21 22:56:24 +0530

    Our Team Responsiveness added

--- src/components/OurTeam.jsx
+++ src/components/OurTeam.jsx
@@ -31,23 +31,23 @@ const OurTeam = () => {
         <>
             <section className="
                 px-[7%]
-                pt-[3rem]
-                pb-[5rem]
+                pt-[2rem] lg:pt-[2rem] xl:pt-[2.5rem] 2xl:pt-[3rem]
+                pb-[4rem] lg:pb-[4rem] xl:pb-[4.5rem] 2xl:pb-[5rem]
             ">
                 <div className="
                     d-flex flex-col items-center
                 ">
                     <h4 className="
-                        text-[4rem]
+                        text-[1rem] lg:text-[3.2rem] xl:text-[3.6rem] 2xl:text-[4rem]
                         font-[600] text-[#0764f9]
                     ">
                         Our Team
                     </h4>
                     <p className="
-                        text-[1.2rem]
+                        text-[1.2rem] lg:text-[0.95rem] xl:text-[1.1rem] 2xl:text-[1.2rem]
                         font-[500] text-[#000]
                         w-[60%]
-                        mt-[1rem]
+                        mt-[1rem] lg:mt-[0.6rem] xl:mt-[0.8rem] 2xl:mt-[1rem]
                         text-center
                     ">
                         Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s
@@ -55,13 +55,13 @@ const OurTeam = () => {
                 </div>
                 <div className="
                     d-flex justify-between flex-wrap
-                    mt-[3rem]
+                    mt-[2rem] lg:mt-[2.3rem] xl:mt-[2.6rem] 2xl:mt-[3rem]
                 ">
                     {
                         teamData.map((item, index) => (
                             <div key={index} className="
                                 w-[31.5%]
-                                rounded-[70px]
+                                rounded-[50px] lg:rounded-[60px] xl:rounded-[65px] 2xl:rounded-[70px]
                                 overflow-hidden
                             ">
                                 <div>
@@ -71,19 +71,19 @@ const OurTeam = () => {
                                 </div>
                                 <div className="
                                     bg-[#0764f9]
-                                    px-[1.3rem]
-                                    py-[2rem]
+                                    px-[1rem] lg:px-[0.9rem] xl:px-[1.1rem] 2xl:px-[1.3rem]
+                                    py-[1.5rem] lg:py-[1.6rem] xl:py-[1.8rem] 2xl:py-[2rem]
                                     text-[#fff]
                                     d-flex flex-col items-center
                                 ">
                                     <span className="
-                                        text-[1.5rem]
+                                        text-[1.5rem] lg:text-[1.3rem] xl:text-[1.4rem] 2xl:text-[1.5rem]
                                         font-[600]
                                     ">
                                         {item.name}
                                     </span>
                                     <span className="
-                                        text-[1rem]
+                                        text-[1rem] lg:text-[0.8rem] xl:text-[0.9rem] 2xl:text-[1rem]
                                         mt-[0.2rem]
                                     ">
                                         {item.designation}
@@ -91,7 +91,7 @@ const OurTeam = () => {
                                     <p className="
                                         text-center
                                         mt-[0.7rem]
-                                        text-[1.1rem]
+                                        text-[1.1rem] lg:text-[0.9rem] xl:text-[1rem] 2xl:text-[1.1rem]
                                     ">
                                         {item.para}
                                     </p>
