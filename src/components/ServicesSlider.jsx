a969d4e3795e81bc13cd329b4ae8855d9e9cbc13
Joseph Pal <josephpal27@gmail.com>
2026-01-26 22:38:03 +0530

    Services Slider Content Updated

--- src/components/ServicesSlider.jsx
+++ src/components/ServicesSlider.jsx
@@ -3,15 +3,13 @@ import { Pagination, Autoplay } from "swiper/modules";
 import "swiper/css";
 import "swiper/css/pagination";
 
-import service1 from '../assets/images/services/1.png';
-
 const ServicesSlider = ({ servicesData }) => {
     return (
         <>
             <Swiper
                 modules={[Pagination, Autoplay]}
                 slidesPerView={4}
-                centeredSlides={true}
+                centeredSlides={false}
                 loop={true}
                 grabCursor={true}
                 spaceBetween={20}
@@ -29,15 +27,40 @@ const ServicesSlider = ({ servicesData }) => {
                 {
                     servicesData.map((item, index) => {
                         return (
-                            <SwiperSlide key={index}>
-                                <div>
-                                    <img src={item.image} alt={item.title} loading="lazy" />
+                            <SwiperSlide key={index} className="
+                                d-flex flex-col items-center
+                                mt-[3.5rem]
+                            ">
+                                <div className="
+                                    bg-white
+                                    p-[2.5rem]
+                                    d-flex justify-center
+                                    aspect-square
+                                    w-[55%]
+                                    rounded-[20px]
+                                    shadow-[0_4px_15px_rgba(0,0,0,0.25)]
+                                    transition
+                                    hover:translate-y-[-5px]
+                                    hover:shadow-[0_4px_15px_#0764f96f]
+                                ">
+                                    <img src={item.image} alt={item.title} loading="lazy" className="
+                                        w-full
+                                    " />
                                 </div>
                                 <div>
-                                    <span>
-                                        {item.title}
-                                    </span>
-                                    <p>
+                                    <span dangerouslySetInnerHTML={{
+                                        __html: item.title,
+                                    }} className="
+                                        text-center text-[#0764f9] font-[600]
+                                        d-block
+                                        text-[1.3rem] lg:text-[1.05rem] xl:text-[1.2rem] 2xl:text-[1.3rem]
+                                        mt-[1.5rem]
+                                    " />
+                                    <p className="
+                                        text-center
+                                        text-[1.1rem] sm:text-[1.2rem] lg:text-[0.95rem] xl:text-[1.1rem] 2xl:text-[1rem]
+                                        mt-[0.8rem]
+                                    ">
                                         {item.desc}
                                     </p>
                                 </div>
