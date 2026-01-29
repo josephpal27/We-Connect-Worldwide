e3af6d27fba072c0ae88331d45119c88ef998026
Joseph Pal <josephpal27@gmail.com>
2026-01-29 12:54:22 +0530

    Footer Mobile View Added

--- src/components/Consultation.jsx
+++ src/components/Consultation.jsx
@@ -4,34 +4,34 @@ const Consultation = () => {
     return (
         <>
             <section className="
-                py-[3rem] lg:py-[4rem] xl:py-[4.5rem] 2xl:py-[5rem]
-                px-[7%]
-                mt-[1rem] lg:mt-[0.8rem] xl:mt-[0.9rem] 2xl:mt-[1rem]
+                py-[2rem] sm:py-[3rem] lg:py-[4rem] xl:py-[4.5rem] 2xl:py-[5rem]
+                px-[1rem] sm:px-[5%] lg:px-[7%]
+                mt-[0.7rem] sm:mt-[1rem] lg:mt-[0.8rem] xl:mt-[0.9rem] 2xl:mt-[1rem]
                 bg-[url('/images/consultation-bg.avif')] bg-cover
                 d-flex flex-col items-center justify-center
             ">
                 <h6 className="
                     text-[#fff] font-[600] text-center
-                    text-[2rem] lg:text-[2.7rem] xl:text-[3.1rem] 2xl:text-[3.5rem]
-                    w-[65%]
+                    text-[1.4rem] sm:text-[2rem] lg:text-[2.7rem] xl:text-[3.1rem] 2xl:text-[3.5rem]
+                    w-full sm:w-[65%]
                 ">
                     Building <span className="text-[#fff]">Smarter</span> Businesses for a Connected <span className="text-[#fff]">World</span>
                 </h6>
                 <p className="
-                    text-[#fff] font-[600] text-center
-                    text-[1.3rem] lg:text-[1.05rem] xl:text-[1.2rem] 2xl:text-[1.3rem]
-                    w-[50%]
-                    mt-[1rem]
+                    text-[#fff] font-[500] sm:font-[600] text-center
+                    text-[1.1rem] sm:text-[1.3rem] lg:text-[1.05rem] xl:text-[1.2rem] 2xl:text-[1.3rem]
+                    w-full sm:w-[50%]
+                    mt-[0.7rem] sm:mt-[1rem]
                 ">
                     Through thoughtful collaboration, we help brands move forward with clarity, confidence, and consistency.
                 </p>
                 <Link to="/contact" className="
-                    text-[#0764f9] bg-[#fff] rounded-[40px] font-[700]
-                    py-[1.2rem] lg:py-[1rem] xl:py-[1.1rem] 2xl:py-[1.2rem]
-                    px-[2.5rem] lg:px-[2.1rem] xl:px-[2.3rem] 2xl:px-[2.5rem]
-                    mt-[2rem] lg:mt-[1.8rem] xl:mt-[1.9rem] 2xl:mt-[2rem]
+                    text-[#0764f9] bg-[#fff] rounded-[40px] font-[550] sm:font-[700]
+                    py-[0.6rem] sm:py-[1.2rem] lg:py-[1rem] xl:py-[1.1rem] 2xl:py-[1.2rem]
+                    px-[1.1rem] sm:px-[2.5rem] lg:px-[2.1rem] xl:px-[2.3rem] 2xl:px-[2.5rem]
+                    mt-[1.5rem] sm:mt-[2rem] lg:mt-[1.8rem] xl:mt-[1.9rem] 2xl:mt-[2rem]
                     mb-[0.4rem]
-                    text-[1.2rem] lg:text-[0.95rem] xl:text-[1.1rem] 2xl:text-[1.2rem]
+                    text-[1.1rem] sm:text-[1.2rem] lg:text-[0.95rem] xl:text-[1.1rem] 2xl:text-[1.2rem]
                     hover:translate-y-[-5px]
                     shadow-[0_5px_15px_rgba(0,0,0,0.35)]
                     transition
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,35 +8,36 @@ const Footer = () => {
     return (
         <>
             <footer className="
-                mt-[1rem] lg:mt-[0.8rem] xl:mt-[0.9rem] 2xl:mt-[1rem]
-                bg-[#0764f9] text-[#fff] px-[7%]
+                mt-[0.7rem] sm:mt-[1rem] lg:mt-[0.8rem] xl:mt-[0.9rem] 2xl:mt-[1rem]
+                bg-[#0764f9] text-[#fff]
+                px-[1rem] sm:px-[5%] lg:px-[7%]
             ">
                 <div className="
                     d-flex justify-between flex-wrap
-                    py-[3rem] lg:py-[3rem] xl:py-[3.5rem] 2xl:py-[4rem]
+                    py-[1.6rem] sm:py-[3rem] lg:py-[3rem] xl:py-[3.5rem] 2xl:py-[4rem]
                 ">
                     <div className="
-                        w-[40%]
+                        w-full sm:w-[40%]
                     ">
                         <div className="
                             bg-[#fff] w-max
                             py-[1rem] lg:py-[0.8rem] xl:py-[0.9rem] 2xl:py-[1rem]
                             px-[1.5rem] lg:px-[1.3rem] xl:px-[1.5rem] 2xl:px-[1.7rem]
-                            rounded-[8] lg:rounded-[6px] xl:rounded-[8px] 2xl:rounded-[10px]
+                            rounded-[8px] lg:rounded-[6px] xl:rounded-[8px] 2xl:rounded-[10px]
                             mt-[0.5rem] lg:mt-[0.4rem] xl:mt-[0.45rem] 2xl:mt-[0.5rem]
-                        "> 
-                            <img src={logo} alt="Logo" loading="lazy" className="w-[100] lg:w-[95px] xl:w-[110px] 2xl:w-[120px]" />
+                        ">
+                            <img src={logo} alt="Logo" loading="lazy" className="w-[90px] lg:w-[95px] xl:w-[110px] 2xl:w-[120px]" />
                         </div>
                         <p className="
-                            mt-[2rem] lg:mt-[1.8rem] xl:mt-[1.9rem] 2xl:mt-[2rem] 
+                            mt-[1.3rem] sm:mt-[2rem] lg:mt-[1.8rem] xl:mt-[1.9rem] 2xl:mt-[2rem] 
                             font-[500]
-                            text-[1.2rem] lg:text-[0.95rem] xl:text-[1.1rem] 2xl:text-[1.2rem]
+                            text-[1.1rem] sm:text-[1.2rem] lg:text-[0.95rem] xl:text-[1.1rem] 2xl:text-[1.2rem]
                         ">
                             We Connect exists to make business journeys smoother and more connected. We work alongside brands to create solutions that feel aligned, effective, and future-ready.
                         </p>
                         <div className="
                             footer-icons
-                            d-flex
+                            hidden sm:flex
                             mt-[1rem] lg:mt-[0.8rem] xl:mt-[0.9rem] 2xl:mt-[1rem]
                             gap-[0.5rem] lg:gap-[0.4rem] xl:gap-[0.5rem] 2xl:gap-[0.6rem]
                         ">
@@ -47,15 +48,16 @@ const Footer = () => {
                         </div>
                     </div>
                     <div className="
-                        w-[50%] 
-                        d-flex justify-end
-                        gap-[30%]
+                        w-full sm:w-[50%] 
+                        d-flex justify-end flex-col sm:flex-row
+                        gap-[0] sm:gap-[30%]
+                        mt-[1.6rem] sm:mt-[0]
                     ">
                         <div>
                             <span className="
                                 d-block font-[700]
-                                text-[1.5rem] lg:text-[1.3rem] xl:text-[1.4rem] 2xl:text-[1.5rem]
-                                mb-[1.5rem] lg:mb-[1.3rem] xl:mb-[1.4rem] 2xl:mb-[1.5rem]
+                                text-[1.3rem] sm:text-[1.5rem] lg:text-[1.3rem] xl:text-[1.4rem] 2xl:text-[1.5rem]
+                                mb-[1rem] sm:mb-[1.5rem] lg:mb-[1.3rem] xl:mb-[1.4rem] 2xl:mb-[1.5rem]
                             ">
                                 Quick Links
                             </span>
@@ -64,11 +66,11 @@ const Footer = () => {
                             <Link to="/about">About Us</Link>
                             <Link to="/contact">Contact</Link>
                         </div>
-                        <div className="contact-info">
+                        <div className="contact-info mt-[1rem] sm:mt-[0]">
                             <span className="
                                 d-block font-[700]
-                                text-[1.5rem] lg:text-[1.3rem] xl:text-[1.4rem] 2xl:text-[1.5rem]
-                                mb-[1.5rem] lg:mb-[1.3rem] xl:mb-[1.4rem] 2xl:mb-[1.5rem]
+                                text-[1.3rem] sm:text-[1.5rem] lg:text-[1.3rem] xl:text-[1.4rem] 2xl:text-[1.5rem]
+                                mb-[1rem] sm:mb-[1.5rem] lg:mb-[1.3rem] xl:mb-[1.4rem] 2xl:mb-[1.5rem]
                             ">
                                 Contact Info
                             </span>
@@ -76,16 +78,27 @@ const Footer = () => {
                             <a href="#"> <FaPhoneAlt className="icn" /> 212-763-3310 (For Visa)</a>
                             <a href="#"> <FaPhoneAlt className="icn" /> 212-222-0992 (For b2b)</a>
                             <a href="https://maps.app.goo.gl/cEYAHQ9hZV7bECNW8" target="_blank"> <FaLocationDot className="icn" /> 117-20, Queens Blvd, 2nd Floor, <br /> Forest Hills, New York - 11375</a>
+                            <div className="
+                                footer-icons
+                                flex sm:hidden
+                                mt-[1.5rem]
+                                gap-[0.7rem]
+                            ">
+                                <a href="#" target="_blank"><FaInstagram /></a>
+                                <a href="#" target="_blank"><FaFacebookF /></a>
+                                <a href="#" target="_blank"><FaLinkedinIn /></a>
+                                <a href="#" target="_blank"><FaYoutube /></a>
+                            </div>
                         </div>
                     </div>
                 </div>
                 <div className="
                     border-t-[1px] border-t-[#ffffff54]
-                    py-[1rem] lg:py-[0.8rem] xl:py-[0.9rem] 2xl:py-[1rem]
+                    py-[0.8rem] sm:py-[1rem] lg:py-[0.8rem] xl:py-[0.9rem] 2xl:py-[1rem]
                 ">
                     <p className="
                         font-[500] text-center
-                        text-[0.9rem] lg:text-[0.7rem] xl:text-[0.8rem] 2xl:text-[0.9rem]
+                        text-[0.85rem] sm:text-[0.9rem] lg:text-[0.7rem] xl:text-[0.8rem] 2xl:text-[0.9rem]
                     ">
                         2025 © All Rights Reserved | We Connect Worldwide | Empowering Businesses Digitally
                     </p>
